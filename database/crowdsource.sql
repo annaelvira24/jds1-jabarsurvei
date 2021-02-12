70a8da4f2554f14866c9b2da964ce90dbad02474
Anna Elvira Hartoyo <naelvirahart@gmail.com>
2021-02-12 21:48:22 +0700

    add survey table on database

--- database/crowdsource.sql
+++ database/crowdsource.sql
@@ -42,6 +42,33 @@ LOCK TABLES `admin` WRITE;
 INSERT INTO `admin` VALUES (1,'example@gmail.com','example','482c811da5d5b4bc6d497ffa98491e38'),(2,'dinkesbdg@gmail.com','Dinas Kesehatan Kota Bandung','96b33694c4bb7dbd07391e0be54745fb'),(3,'disdikjabar@gmail.com','Dinas Pendidikan Jawa Barat','7d347cf0ee68174a3588f6cba31b8a67');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `survey`
+--
+
+DROP TABLE IF EXISTS `survey`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `survey` (
+  `id_survey` int NOT NULL,
+  `id_admin` int NOT NULL,
+  `survey_title` varchar(255) DEFAULT NULL,
+  `decription` varchar(4096) DEFAULT NULL,
+  PRIMARY KEY (`id_survey`),
+  KEY `id_admin` (`id_admin`),
+  CONSTRAINT `survey_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey`
+--
+
+LOCK TABLES `survey` WRITE;
+/*!40000 ALTER TABLE `survey` DISABLE KEYS */;
+/*!40000 ALTER TABLE `survey` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-10 21:20:43
+-- Dump completed on 2021-02-12 21:47:50
