e61f1199a087f4721e52118f5fd1b9979d8f32e1
Andjani Kiranadewi <13518109@std.stei.itb.ac.id>
2021-02-12 20:08:01 +0700

    Changed html tags

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -1,43 +1,42 @@
 import React from 'react';
-import { Button, Container, Col, Row, Alert } from 'react-bootstrap';
 
 function Demo(){
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Button variant="t-green">Green</Button>
-                </Col>
-                <Col>
-                    <Button variant="t-yellow">Yellow</Button>
-                </Col>
-                <Col>
-                <Button variant="t-blue">Blue</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Button variant="outline-t-green">Green</Button>
-                </Col>
-                <Col>
-                    <Button variant="outline-t-yellow">Yellow</Button>
-                </Col>
-                <Col>
-                <Button variant="outline-t-blue">Blue</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Alert variant="t-green">Green</Alert>
-                </Col>
-                <Col>
-                    <Alert variant="t-yellow">Yellow</Alert>
-                </Col>
-                <Col>
-                    <Alert variant="t-blue">Green</Alert>
-                </Col>
-            </Row>
-        </Container>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <button type="button" className="btn btn-t-green">Green</button>
+                </div>
+                <div className="col">
+                    <button type="button" className="btn btn-t-yellow">Yellow</button>
+                </div>
+                <div className="col">
+                    <button type="button" className="btn btn-t-blue">Blue</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <button type="button" className="btn btn-outline-t-green">Green</button>
+                </div>
+                <div className="col">
+                    <button type="button" className="btn btn-outline-t-yellow">Yellow</button>
+                </div>
+                <div className="col">
+                    <button type="button" className="btn btn-outline-t-blue">Blue</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <div className="alert alert-t-green">Green</div>
+                </div>
+                <div className="col">
+                    <div className="alert alert-t-yellow">Yellow</div>
+                </div>
+                <div className="col">
+                    <div className="alert alert-t-blue">Green</div>
+                </div>
+            </div>
+        </div>
         
     )
 }
