41c850a8d34d3aa787107989f28f38a442143024
Andjani Kiranadewi <13518109@std.stei.itb.ac.id>
2021-02-13 14:36:22 +0700

    Organized .scss files into one folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import './App.scss';
+import './assets/scss/App.scss';
 import Navigation from './components/Navigation';
 import Demo from './components/Demo';
 
