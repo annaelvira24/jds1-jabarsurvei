8c218f8697010fb34483f829e547d07b2d2a8f24
Andjani Kiranadewi <13518109@std.stei.itb.ac.id>
2021-03-01 17:07:17 +0700

    Pagination feature

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -5,13 +5,21 @@ import Carousel_dash from '../components/Carousel_dash.js';
 import Card_dash from '../components/Card_dash.js';
 import '../assets/css/LandingPage.css';
 import http from "../http-common";
-
+import PaginationButton from '../components/Pagination'
 
 class LandingPage extends Component {
-
-  state = {
-    listSurvey: []
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      listSurvey: [],
+      display: [],
+      offset: 0,
+      currentPage: 1,
+      perPage: 5,
+      pageCount: 0
+    };
+    this.handlePageClick = this.handlePageClick.bind(this)
+  }
 
   static get coloumns() { 
       return (
@@ -53,8 +61,10 @@ class LandingPage extends Component {
                 </Row>
             </Container>
         </header>
-        <body className="Survey-Container">
-            <Table_comp daftar_survey={this.state.listSurvey}  daftar_coloumn={this.constructor.coloumns}/>
+        <body className="Survey-Container border">
+          <PaginationButton totalPage={this.state.pageCount} pageMargin={1} onPageClick={this.handlePageClick} currentPage={this.state.currentPage} className='mx-auto' />
+          <Table_comp daftar_survey={this.state.display}  daftar_coloumn={this.constructor.coloumns}/>
+          <PaginationButton totalPage={this.state.pageCount} pageMargin={1} onPageClick={this.handlePageClick} currentPage={this.state.currentPage} className='mx-auto' />
         </body>
       </div>
     );
@@ -63,13 +73,43 @@ class LandingPage extends Component {
   componentDidMount() {
     http.get('http://localhost:5000/api/listSurvey/findAll')
       .then(res => {
-        
+        console.log("Mounted")
         const listSurvey = res.data;
-        this.setState({ listSurvey });
-        console.log(listSurvey);
-      })
-    
+        const count = Math.ceil(listSurvey.length/this.state.perPage)
+        const display = listSurvey.slice(0, this.state.perPage)
+
+        this.setState({ 
+          listSurvey,
+          display,
+          pageCount: count,
+          currentPage: 1
+        });
+      }) 
   }
 
+  handlePageClick(e) {
+    // Change active page
+    const current = this.state.currentPage
+    const total = this.state.pageCount
+
+    var button = e.target.text
+
+    if (button == undefined) return // Error?
+    if (button == '‹Previous') {
+      button = current == 1 ? 1 : current-1
+    } else if (button == '›Next') {
+      button = current == total ? total : current+1
+    }
+
+    const offset = (parseInt(button)-1)*this.state.perPage
+    const display = this.state.listSurvey.slice(offset, offset+this.state.perPage)
+
+    this.setState({
+      display: display,
+      currentPage: parseInt(button),
+    })
+    console.log(this.state)
+  }
 };
-  export default LandingPage;
+  
+export default LandingPage;
