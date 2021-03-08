6feb0df2c63a132ca9d0e0dca71616f57564fdea
Andjani Kiranadewi <13518109@std.stei.itb.ac.id>
2021-03-08 19:32:12 +0700

    Add Form Builder styling

--- frontend/src/views/FormBuilder.js
+++ frontend/src/views/FormBuilder.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import React, { Component, createRef } from "react";
 import http from "../http-common";
 import 'jquery-ui-sortable';
+import './../assets/scss/FormBuilder.scss'
 
 window.jQuery = $;
 window.$ = $;
@@ -35,7 +36,8 @@ class FormBuilder extends Component {
     }
     
     componentDidMount() {
-      this.state.idSurvey = this.props.match.params.id;
+      if (this.props.match)
+        this.state.idSurvey = this.props.match.params.id;
 
       // create NEW survey
       if(this.state.idSurvey == undefined){
@@ -53,116 +55,116 @@ class FormBuilder extends Component {
  
       $(this.fbBuilder.current).formBuilder({
         formData: formDataTemp,
-        disabledActionButtons: ['clear','save','data'], 
+        disabledActionButtons: ['clear','save'], 
         disableFields: ['autocomplete','button', 'hidden'],
         disabledAttrs: ['name', 'access', 'className', 'value', 'maxlength'],
         i18n: {
           override: {
             'en-US': {
               addOption: 'Tambah Opsi +',
-            allFieldsRemoved: 'All fields were removed.',
-            allowMultipleFiles: 'Perbolehkan pengguna untuk upload banyak file',
-            autocomplete: 'Autocomplete',
-            button: 'Tombol',
-            cannotBeEmpty: 'Tidak boleh kosong',
-            checkboxGroup: 'Checkboxes',
-            className: 'Class',
-            clearAllMessage: 'Are you sure you want to clear all fields?',
-            clear: 'Hapus semua',
-            close: 'Tutup',
-            content: 'Konten',
-            copy: 'Salin Ke Clipboard',
-            copyButton: '&#43;',
-            copyButtonTooltip: 'Salin',
-            dateField: 'Pilih Tanggal',
-            description: 'Teks Bantuan',
-            descriptionField: 'Deskripsi',
-            devMode: 'Mode Pengembang',
-            editNames: 'Edit Names',
-            editorTitle: 'Form Elements',
-            editXML: 'Edit XML',
-            enableOther: 'Opsi Lainnya',
-            enableOtherMsg: 'Perbolehkan pengguna untuk mengisi opsi yang tidak terdaftar',
-            fieldNonEditable: 'Field ini tidak bisa di-edit.',
-            fieldRemoveWarning: 'Are you sure you want to remove this field?',
-            fileUpload: 'Upload File',
-            formUpdated: 'Form Diperbaharui',
-            getStarted: 'Tarik field dari kanan ke area ini',
-            header: 'Header',
-            hide: 'Edit',
-            hidden: 'Input Tersembunyi',
-            inline: 'Inline',
-            inlineDesc: 'Tampilkan {type} inline',
-            label: 'Label',
-            labelEmpty: 'Field Label cannot be empty',
-            limitRole: 'Batasi akses ke satu atau lebih dari role berikut:',
-            mandatory: 'Wajib',
-            maxlength: 'Panjang Maksimum',
-            minOptionMessage: 'Dibutuhkan setidaknya 2 opsi',
-            multipleFiles: 'File Jamak',
-            name: 'Nama',
-            no: 'Tidak',
-            noFieldsToClear: 'Tidak ada field untuk dibersihkan',
-            number: 'Angka',
-            off: 'Off',
-            on: 'On',
-            option: 'Pilihan',
-            options: 'Pilihan',
-            optional: 'Opsional',
-            optionLabelPlaceholder: 'Label',
-            optionValuePlaceholder: 'Nilai',
-            optionEmpty: 'Nilai opsi dibutuhkan',
-            other: 'Lainnya',
-            paragraph: 'Paragraf',
-            placeholder: 'Placeholder',
-            'placeholder.value': 'Nilai',
-            'placeholder.label': 'Label',
-            'placeholder.text': '',
-            'placeholder.textarea': '',
-            'placeholder.email': 'Isi alamat email anda',
-            'placeholder.placeholder': '',
-            'placeholder.className': 'space separated classes',
-            'placeholder.password': 'Isi kata sandi',
-            preview: 'Preview',
-            radioGroup: 'Radio Group',
-            radio: 'Radio',
-            removeMessage: 'Hapus Elemen',
-            removeOption: 'Hapus Opsi',
-            remove: '&#215;',
-            required: 'Wajib diisi',
-            richText: 'Rich Text Editor',
-            roles: 'Akses',
-            rows: 'Baris',
-            save: 'Simpan',
-            selectOptions: 'Opsi',
-            select: 'Select',
-            selectColor: 'Pilih Warna',
-            selectionsMessage: 'Perbolehkan banyak pilihan',
-            size: 'Ukuran',
-            'size.xs': 'Sangat Kecil',
-            'size.sm': 'Kecil',
-            'size.m': 'Sedang',
-            'size.lg': 'Besar',
-            style: 'Style',
-            styles: {
-              btn: {
-                default: 'Default',
-                danger: 'Bahaya',
-                info: 'Info',
-                primary: 'Utama',
-                success: 'Sukses',
-                warning: 'Peringatan'
-              }
-            },
-            subtype: 'Tipe',
-            text: 'Text Field',
-            textArea: 'Text Area',
-            toggle: 'Toggle',
-            warning: 'Peringatan!',
-            value: 'Value',
-            viewJSON: 'View JSON',
-            viewXML: '&lt;/&gt;',
-            yes: 'Ya'
+              allFieldsRemoved: 'All fields were removed.',
+              allowMultipleFiles: 'Perbolehkan pengguna untuk upload banyak file',
+              autocomplete: 'Autocomplete',
+              button: 'Tombol',
+              cannotBeEmpty: 'Tidak boleh kosong',
+              checkboxGroup: 'Checkboxes',
+              className: 'Class',
+              clearAllMessage: 'Are you sure you want to clear all fields?',
+              clear: 'Hapus semua',
+              close: 'Tutup',
+              content: 'Konten',
+              copy: 'Salin Ke Clipboard',
+              copyButton: '&#43;',
+              copyButtonTooltip: 'Salin',
+              dateField: 'Pilih Tanggal',
+              description: 'Teks Bantuan',
+              descriptionField: 'Deskripsi',
+              devMode: 'Mode Pengembang',
+              editNames: 'Edit Names',
+              editorTitle: 'Form Elements',
+              editXML: 'Edit XML',
+              enableOther: 'Opsi Lainnya',
+              enableOtherMsg: 'Perbolehkan pengguna untuk mengisi opsi yang tidak terdaftar',
+              fieldNonEditable: 'Field ini tidak bisa di-edit.',
+              fieldRemoveWarning: 'Are you sure you want to remove this field?',
+              fileUpload: 'Upload File',
+              formUpdated: 'Form Diperbaharui',
+              getStarted: 'Tarik field dari kanan ke area ini',
+              header: 'Header',
+              hide: 'Edit',
+              hidden: 'Input Tersembunyi',
+              inline: 'Inline',
+              inlineDesc: 'Tampilkan {type} inline',
+              label: 'Label',
+              labelEmpty: 'Field Label cannot be empty',
+              limitRole: 'Batasi akses ke satu atau lebih dari role berikut:',
+              mandatory: 'Wajib',
+              maxlength: 'Panjang Maksimum',
+              minOptionMessage: 'Dibutuhkan setidaknya 2 opsi',
+              multipleFiles: 'File Jamak',
+              name: 'Nama',
+              no: 'Tidak',
+              noFieldsToClear: 'Tidak ada field untuk dibersihkan',
+              number: 'Angka',
+              off: 'Off',
+              on: 'On',
+              option: 'Pilihan',
+              options: 'Pilihan',
+              optional: 'Opsional',
+              optionLabelPlaceholder: 'Label',
+              optionValuePlaceholder: 'Nilai',
+              optionEmpty: 'Nilai opsi dibutuhkan',
+              other: 'Lainnya',
+              paragraph: 'Paragraf',
+              placeholder: 'Placeholder',
+              'placeholder.value': 'Nilai',
+              'placeholder.label': 'Label',
+              'placeholder.text': '',
+              'placeholder.textarea': '',
+              'placeholder.email': 'Isi alamat email anda',
+              'placeholder.placeholder': '',
+              'placeholder.className': 'space separated classes',
+              'placeholder.password': 'Isi kata sandi',
+              preview: 'Preview',
+              radioGroup: 'Radio Group',
+              radio: 'Radio',
+              removeMessage: 'Hapus Elemen',
+              removeOption: 'Hapus Opsi',
+              remove: '&#215;',
+              required: 'Wajib diisi',
+              richText: 'Rich Text Editor',
+              roles: 'Akses',
+              rows: 'Baris',
+              save: 'Simpan',
+              selectOptions: 'Opsi',
+              select: 'Select',
+              selectColor: 'Pilih Warna',
+              selectionsMessage: 'Perbolehkan banyak pilihan',
+              size: 'Ukuran',
+              'size.xs': 'Sangat Kecil',
+              'size.sm': 'Kecil',
+              'size.m': 'Sedang',
+              'size.lg': 'Besar',
+              style: 'Style',
+              styles: {
+                btn: {
+                  default: 'Default',
+                  danger: 'Bahaya',
+                  info: 'Info',
+                  primary: 'Utama',
+                  success: 'Sukses',
+                  warning: 'Peringatan'
+                }
+              },
+              subtype: 'Tipe',
+              text: 'Isian Singkat',
+              textArea: 'Text Area',
+              toggle: 'Toggle',
+              warning: 'Peringatan!',
+              value: 'Value',
+              viewJSON: 'View JSON',
+              viewXML: '&lt;/&gt;',
+              yes: 'Ya'
             }
           }
         }
@@ -213,16 +215,22 @@ class FormBuilder extends Component {
             <div id="fb-editor" ref={this.fbBuilder}>
 
             </div>
-            <button id="render" onClick={this.handleClearBuilder.bind(this)}>Bersihkan</button>
-            <button id="clear" onClick={this.handlePreviewEdit.bind(this)}>Tampilan</button>
+            <div id="builder-button-container">
+              <button id="render" onClick={this.handleClearBuilder.bind(this)} className="btn btn-outline-secondary">Bersihkan</button>
+              <button id="clear" onClick={this.handlePreviewEdit.bind(this)} className="btn btn-outline-secondary">Tampilan</button>
+            </div>
           </div>
           <div id="fb-rendered-form" ref={this.fbRenderWrapper}>
             <div id="fb-rendered" ref={this.fbRender}>
 
             </div>
-            <button id="render" onClick={this.handlePreviewEdit.bind(this)}>Edit kembali</button>
-            <button id="save" onClick={this.handleSaveForm.bind(this)}>Simpan</button>
+            <div id="render-button-container clearfix">
+              <button id="render" onClick={this.handlePreviewEdit.bind(this)} className="btn btn-outline-secondary float-left">Edit kembali</button>
+              <button id="save" onClick={this.handleSaveForm.bind(this)} className="btn btn-outline-success float-right">Simpan</button>
+            </div>
+            
           </div>
+          
         </div>
       );
     }
