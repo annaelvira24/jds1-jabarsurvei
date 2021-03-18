d09df80714389cfb34e8d6b2fa7eec7c567e2bab
Filbert Wijaya <13518077@std.stei.itb.ac.id>
2021-03-18 23:40:09 +0700

    initialise show survey

--- frontend/src/views/Survey.js
+++ frontend/src/views/Survey.js
@@ -1,13 +1,84 @@
-import React, { Component } from 'react';
+import $ from "jquery";
+import React, { Component, createRef } from "react";
+import http from "../http-common";
+import { getUser } from './../util/Common.js';
+import 'jquery-ui-sortable';
+import './../assets/scss/FormBuilder.scss'
+
+window.jQuery = $;
+window.$ = $;
+
+require('formBuilder');
+require('formBuilder/dist/form-render.min.js');
+
+var formDataTemp = [];
 
 
 
-function Survey() {
+class Survey extends Component {
+    fbRender = createRef();
 
-    return (
+    state = {
+      cookie: undefined,
+      idSurvey : undefined,
+      idAdmin : undefined
+    }
 
-        <p>Haii</p>
-    );
-  }
+    constructor(){
+      super();
+      this.state.cookie = getUser();
+      this.state.idAdmin = JSON.parse(atob(this.state.cookie))[0].id_admin;
+
+      //this.handleSaveForm = this.handleSaveForm.bind(this);
+    }
+    
+    componentDidMount() {
+      if (this.props.match)
+        this.state.idSurvey = this.props.match.params.id;
+
+      // edit existing survey
+      if(this.state.idSurvey !== undefined){
+        /* http.get('http://localhost:5000/api/surveyFill/getDescription/' + this.state.idSurvey)
+        .then(res => {
+            console.log(res.data.survey_title);
+            
+        }); */
+        http.get('http://localhost:5000/api/surveyFill/findById/' + this.state.idSurvey)
+        .then(res => {
+          for (var i = 0; i<res.data.length; i++){
+            formDataTemp.push(JSON.parse(res.data[i].details));
+          }
+        });
+      }
+      $(this.fbRender.current).formRender({
+        dataType: 'json',
+        formData:  formDataTemp
+      });
+    }
+    render() {
+        return(
+          <div id = "surveyContainer">
+            <div id = "surveyTitle">
+              <div className="form-group">
+                <span type="text" id="title-input"/>
+                <br/>
+                <span type="text" id="description-input" />
+              </div>
   
-  export default Survey;
+            </div>
+  
+            <div id="surveyMain">
+              <div id="fb-rendered-form">
+                <div id="fb-rendered" ref={this.fbRender}>
+  
+                </div>
+                
+              </div>
+              
+            </div>
+          </div>
+        );
+      }
+}
+  
+export default Survey;
