7ed0fea1b0788555d9b522871b5e79f96256f71e
Anna Elvira Hartoyo <naelvirahart@gmail.com>
2021-04-15 17:48:58 +0700

    responsive navbar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Dropdown } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { Component } from 'react';
 import Logo from './../assets/image/logo.png';
 import { getUser, removeUserCookie } from './../util/Common.js';
@@ -20,36 +20,35 @@ class Navigation extends Component{
         e.preventDefault();
         removeUserCookie();
         window.location.href = "/";
-
     }
     
     render() {
         return (
-            <Navbar id = "sticky-navbar">
-                <Nav className="container-fluid">
-                    <Nav.Item>
-                        <Navbar.Brand>
-                            <a href="/">
-                            <img src = {Logo}/>
-                            </a>
-                        </Navbar.Brand>
-                    </Nav.Item>
-                    <Navbar.Collapse className="justify-content-end">
-                        {(this.state.cookie)
-                            && (<Nav.Link href="/dashboard">Beranda</Nav.Link>
-                        )}
-                        {(!this.state.cookie)
-                            && (<Nav.Link href="/login">Masuk</Nav.Link>
-                        )}
-                        {(this.state.cookie)
-                            && (<Nav.Link onClick= {(e)=> this.handleLogout(e)}>Keluar</Nav.Link>
-                        )}
-                    </Navbar.Collapse>
-                </Nav>
+            <Navbar collapseOnSelect expand="lg" id = "sticky-navbar">
+                <Navbar.Brand>
+                    <a href="/">
+                    <img src = {Logo}/>
+                    </a>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="container-fluid">
+                        <Navbar.Collapse className="justify-content-end">
+                            {(this.state.cookie)
+                                && (<Nav.Link href="/dashboard">Beranda</Nav.Link>
+                            )}
+                            {(!this.state.cookie)
+                                && (<Nav.Link href="/login">Masuk</Nav.Link>
+                            )}
+                            {(this.state.cookie)
+                                && (<Nav.Link onClick= {(e)=> this.handleLogout(e)}>Keluar</Nav.Link>
+                            )}
+                        </Navbar.Collapse>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         );
-    }
-    
+    } 
 };
 
 export default Navigation;
